cedacd61bc4b8b155ea7d325c6502258d3a108e9
Lluís Barbó López <107318977+barbosoft@users.noreply.github.com>
2025-08-29 10:19:20 +0200

    Open library formulari llibre desitjat (#1)
    * Actualitzat .gitignore per cobrir IntelliJ, Gradle, logs, temporals i configuracions locals
    
    * Actualitzat .gitignore per cobrir IntelliJ, Gradle, logs, temporals i configuracions locals
    
    * open library, nou formulari per comprar (llista de desitjos)

--- src/main/java/com/barbosoft/biblioteca/bibliotecaapi/config/SecurityConfig.java
+++ src/main/java/com/barbosoft/biblioteca/bibliotecaapi/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,7 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(crsf -> crsf.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/api/llibres/**").permitAll()
